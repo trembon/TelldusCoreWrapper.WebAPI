271a1e0616562826d66949c91889c3b572425cee
trembon <trembon@hotmail.com>
2019-11-01 22:33:59 +0100

    upgraded to dotnet core 3.0

--- TelldusCoreWrapper.WebAPI/Startup.cs
+++ TelldusCoreWrapper.WebAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using TelldusCoreWrapper.WebAPI.Services;
@@ -22,7 +23,6 @@ namespace TelldusCoreWrapper.WebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLogging(options =>
@@ -35,11 +35,10 @@ namespace TelldusCoreWrapper.WebAPI
             services.AddSingleton<ITelldusCoreService, TelldusCoreService>();
             services.AddSingleton<ITelldusEventService, TelldusEventService>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ITelldusEventService telldusEventService, ITelldusCoreService telldusCoreService)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ITelldusEventService telldusEventService, ITelldusCoreService telldusCoreService)
         {
             if (env.IsDevelopment())
             {
@@ -49,7 +48,11 @@ namespace TelldusCoreWrapper.WebAPI
             telldusCoreService.InitializeInThread(3000);
             telldusEventService.Initialize();
 
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
